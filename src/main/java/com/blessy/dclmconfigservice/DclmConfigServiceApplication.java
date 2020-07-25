8c34a71168f9e4870441cb09743b7c6eb7d91de0
Mothusi Molorane <molorane.mothusi@gmail.com>
2020-07-25 19:28:23 +0200

    first commit

---
+++ src/main/java/com/blessy/dclmconfigservice/DclmConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blessy.dclmconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DclmConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DclmConfigServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blessy/dclmconfigservice/DclmConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blessy.dclmconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DclmConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
